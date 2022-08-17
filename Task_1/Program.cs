36f21c8ab8e0cff50956e5ec688f1c74ecd5a0a4
«Kristina» <kristinabol1996@gmail.com>
2022-08-18 05:18:50 +0800

    1 задача, возведение числа А в степень В

---
+++ Task_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Цикл, который принимает на вход два числа (A, B) и возводит 
+// число А в натуральную степень В.
+
+int NaturalDegree(int A, int B)
+{
+    int result = 1;
+    for (int i = 0; i < B; i++)
+    {
+        result *= A;
+    }
+    return result;
+}
+
+void PrintNaturalDegree(int A, int B)
+{
+    Console.WriteLine($"{A} в степени {B} = {NaturalDegree(A, B)}");
+}
+PrintNaturalDegree(3, 5);
+PrintNaturalDegree(2, 4);
