0b5bbf211c3b1c5d3d13edce23f82a99c778f4e5
«Kristina» <kristinabol1996@gmail.com>
2022-08-18 05:36:29 +0800

    3 задача, создание массива из рандомных чисел

---
+++ Task_3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Программа, которая задает массив из 8 элементов и выводит их на экран
+
+int[] Array(int len)
+{
+    int[] arr = new int[len];
+    for (int i = 0; i < len; i++)
+    {
+        arr[i] = new Random().Next(0,30);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[ ");
+    int len = arr.Length;
+    for (int i = 0; i < len; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.Write("]");
+}
+
+PrintArray(Array(8));
