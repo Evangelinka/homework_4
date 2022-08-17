a75c0605cc4762839e92878d9cd28a80276115a2
«Kristina» <kristinabol1996@gmail.com>
2022-08-18 05:17:35 +0800

    2 задача, сумма цифр в числе

---
+++ Task_2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Программа, которая принимает на вход число и выдает сумму цифр в числе.
+
+int SumDigits(int N)
+{
+    int str_count = Convert.ToString(N).Length;
+    int digit_count = 0;
+    int result = 0;
+    for(int i = 0; i < str_count; i++)
+    {
+        digit_count = N - N % 10;
+        result += N - digit_count;
+        N /= 10;
+    }
+    return result;
+}
+
+void PrintSumDigits(int N)
+{
+    Console.WriteLine($"Сумма цифр в числе {N} = {SumDigits(N)}");
+}
+
+PrintSumDigits(452);
+PrintSumDigits(82);
+PrintSumDigits(9012);
